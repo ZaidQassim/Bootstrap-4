91117e3d29fd439712b815ccf1e4012069433021
Zaid Qassim <zaidqassim12@gmail.com>
2020-04-28 03:40:43 +0300

    design bootsrap 4

---
+++ js/main.js
@@ -0,0 +1,33 @@
+
+// to calulation the height slider 
+$(function () {
+    'use strict';
+
+    var winH = $(window).height(),
+        upperH = $('.upper-bar').innerHeight(),
+        navH = $('.navbar').innerHeight();
+
+   $('.slider, .carousel-item').height(winH - (upperH + navH ));   
+
+   // Featured Work  Shuffle 
+   $('.featured-work ul li').on('click', function () {
+
+       $(this).addClass('active').siblings().removeClass('active');
+       
+        // console.log($(this).data('class'))
+
+       if ($(this).data('class') === 'all') {
+           $('.shuffle-imgs .col-sm-3').css('opacity', 1);         
+       } 
+       else {
+         $('.shuffle-imgs .col-sm-3').css('opacity', '.08');
+         $($(this).data('class')).parent().css('opacity', 1);
+       } 
+   });
+   
+ 
+
+
+
+});
+
